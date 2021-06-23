e2ed99a5d1ff3dfe3d224f80a54efc5ff6ff685c
Natalia <natalia.sochacka@atos.net>
2021-06-23 16:16:47 +0200

    Corrected RabbitHandlerUnitTest

--- src/test/java/net/atos/server/RabbitHandlerUnitTest.java
+++ src/test/java/net/atos/server/RabbitHandlerUnitTest.java
@@ -3,18 +3,28 @@ package net.atos.server;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.embedded.EmbeddedChannel;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
-//@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = EmbeddedChannel.class)
 public class RabbitHandlerUnitTest {
 
     EmbeddedChannel embeddedChannel = new EmbeddedChannel(new RabbitHandler());
+
+    @Test
+    public void getRabbitTemplateTest(){
+
+        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+        RabbitServerHandler rabbitServerHandler = new RabbitServerHandler(rabbitTemplate);
+
+        assertEquals(rabbitTemplate, rabbitServerHandler.getRabbitTemplate());
+    }
 
     @Test
     public void testHandler(){
